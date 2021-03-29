99c547aed040300689ebc3ac82d0dbb82e96381c
ggarber42 <ggarber@protonmail.com>
2021-03-29 09:11:12 -0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+       <b-navbar toggleable="lg" type="dark" variant="info">
+      <h3><b>DJANGO</b> + <b>VUE</b> = <b>CRUD DA MASSA</b></h3>
+       </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+    h3{
+        color: #fff;
+    }
+
+</style>>
